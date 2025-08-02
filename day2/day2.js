87f9d1847a1771af1d415527147a2606ec743bab
Dhruv Pancholi <160162577+dhruv457457@users.noreply.github.com>
2025-08-02 20:15:53 +0530

    day2 done

---
+++ day2/day2.js
@@ -0,0 +1,57 @@
+// // alert prompt confirm 
+
+
+// result=prompt('how old are you');
+// alert(`you are ${result} years old`);   //prompt for input
+
+// let isBoss = confirm("are you king");  //confirm for yes or no
+// alert(isBoss);
+
+
+
+// let a = prompt("put val a",1);
+// let b = prompt("put val b",3);
+// alert(Number(a)+Number(b));
+
+
+
+// // if and else 
+
+// let ans = prompt("What is the official name of js","namee");
+// if(ans=="ECMAScript"||ans=="ecmascript"){
+//     alert("You are correct");
+// }
+// else {
+//     alert("you are dumb");
+// } 
+
+// // ? USING replace of if else
+//     let ressult;
+// ressult= (a+b<4) ?'Bellpw':"over";
+// alert(ressult); 
+
+// // great logical question 
+// alert(alert(1)|| 2 || alert(3) );
+
+// let a=0;
+// if(a>14 && a<90){
+//     console.log("inculsive");
+
+// }
+// else{console.log("no");}
+
+// // login using prompts if operators
+
+// let U_login =prompt("Who are you","Role");
+// if(U_login=="Role"){
+//     alert("You are wellcome please click on ok ");
+//     let pass=prompt("write password","go ahead");
+//     if(pass=="Cool"){
+//         alert("wellcome")
+//     }
+//     else{alert("wrong password");}
+
+// }
+// else {
+//     alert("I dont know you");
+// }
